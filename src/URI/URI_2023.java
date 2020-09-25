32f2f6609a46c707e1285f9b66ad1db54d0804f5
Matheus Ferreira <matheus_fsilva05@hotmail.com>
2020-09-24 22:35:57 -0300

    melhoria questao 2023

--- src/URI/URI_2023.java
+++ src/URI/URI_2023.java
@@ -1,29 +1,23 @@
 package URI;
 
-import java.util.Map;
 import java.util.Scanner;
-import java.util.TreeMap;
 
 public class URI_2023 {
 
 	public static void main(String[] args) {
 		
 		Scanner sc = new Scanner(System.in);
-		Map<String, String> map = new TreeMap<>();
 				
+		String str = "";
+		String name;
 		while (sc.hasNext()) {
-			String name = sc.nextLine();
-			map.put(name.toLowerCase(), name);			
-		}
-		
-		int i = 0;		
-		for (String key : map.keySet()) {
-			if (i == map.size()-1) {
-				System.out.println(map.get(key));
+			name = sc.nextLine();	
+			if (name.compareToIgnoreCase(str) > 0) {
+				str = name;
 			}
-			i++;
 		}
 		
+		System.out.println(str);
 		
 		sc.close();
 	}
